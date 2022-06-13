752e441d6742ca3a26b7d45f2382c007442a7c96
AnaLama <analama3744@gmail.com>
2022-06-13 13:36:47 -0400

    "Test name changed"

--- src/test/java/tests/AnaLamaFooterMenuBarButtonsTest.java
+++ src/test/java/tests/AnaLamaFooterMenuBarButtonsTest.java
@@ -10,7 +10,7 @@ public class AnaLamaFooterMenuBarButtonsTest extends BaseTest {
     private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
 
     @Test
-    public void testH1HeaderMainPage() {
+    public void testFooterMenuBarMainPage() {
 
         String expectedResult = "Start | Browse Languages | Search Languages | Top Lists | Guestbook | Submit new Language";
 
