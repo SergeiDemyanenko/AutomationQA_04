7d5e8f86a50d8c74fc3a826001763a05e737dd9c
GalaKon <gala.kon13@gmail.com>
2022-06-15 17:32:41 +0400

    Check the name of Mathematica Language's author

--- src/test/java/pages/MPage.java
+++ src/test/java/pages/MPage.java
@@ -1,10 +1,16 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class MPage {
 
     private WebDriver driver;
+
+    private final By M_PAGE = By.xpath("//a[@href='m.html']");
+    private final By MATHEMATICA_PAGE = By.xpath("//a[@href='language-mathematica-1090.html']");
+    private final By BRENTON_BOSTICK = By.xpath("//table[@style='margin: 7px; padding: 0;;']/tbody[1]/tr[2]/td[2]");
 
     public MPage(WebDriver existingDriver) {
 
@@ -14,5 +20,20 @@ public class MPage {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    public WebElement MLanguagesPage() {
+
+         return getDriver().findElement(M_PAGE);
+    }
+
+    public WebElement mathematicaPage() {
+
+        return getDriver().findElement(MATHEMATICA_PAGE);
+    }
+
+    public WebElement BrentonBostick() {
+
+        return getDriver().findElement(BRENTON_BOSTICK);
     }
 }
