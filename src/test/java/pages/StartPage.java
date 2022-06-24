be5bf1ed6fb26cbbd0839154bcd4c21d5cca60fa
Ekaterina Kazakova <kazakova.e.v@gmail.com>
2022-06-24 18:12:17 +0300

    200.021 [Start] Text Information about the Website | Wishes from the Team | create locator and methods

--- src/test/java/pages/StartPage.java
+++ src/test/java/pages/StartPage.java
@@ -12,6 +12,8 @@ public class StartPage {
     private final By SONG_LYRICS_MENU = By.linkText("Song Lyrics");
     private final By HISTORY_MENU = By.xpath("By.xpath(//a[@href='info.html']']);");
 
+    private final By P_WISHES_START_PAGE = By.xpath("//div[@id='main']/p[last()]");
+
     public StartPage(WebDriver existingDriver) {
 
         this.driver = existingDriver;
@@ -60,4 +62,13 @@ public class StartPage {
 
         getHistoryMenu().click();
     }
+
+    public WebElement getTeamWishes() {
+        return getDriver().findElement(P_WISHES_START_PAGE);
+    }
+
+    public String getTeamWishesText() {
+        return getTeamWishes().getText();
+    }
+
 }
