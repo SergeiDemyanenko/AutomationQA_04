f1b2cf23c2c1627229031407ca9db877f487ffc9
GalaKon <gala.kon13@gmail.com>
2022-06-26 18:06:01 +0400

    Empty commit to resolve a problem

--- src/test/java/pages/AbcPage.java
+++ src/test/java/pages/AbcPage.java
@@ -18,7 +18,6 @@ public class AbcPage {
     private final By CATEGORY_09 = By.xpath("//a[@href='0.html']");
     private final By ALL_CATEGORIES = By.xpath("//ul[@id='submenu']//a");
     private final By CATEGORY_R = By.xpath("//a[@href='r.html']");
-    private final By SUBMENU_CATEGORIES = By.xpath("//ul[@id='submenu']");
 
     public AbcPage(WebDriver existingDriver) {
 
@@ -94,10 +93,5 @@ public class AbcPage {
     public WebElement getCategoryR() {
 
         return getDriver().findElement(CATEGORY_R);
-    }
-
-    public WebElement getSubmenuCategories(){
-
-        return getDriver().findElement(SUBMENU_CATEGORIES);
     }
 }
