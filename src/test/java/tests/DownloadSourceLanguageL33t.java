1b59769844ca313e243a74d658d8b4f9ea5d9093
Arailym Ashimova <arajlymasimova@gmail.com>
2022-06-28 21:43:55 +0600

    add test DownloadSourceLanguageL33t

--- src/test/java/tests/DownloadSourceLanguageL33t.java
+++ src/test/java/tests/DownloadSourceLanguageL33t.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LPage;
 import pages.MainPage;
@@ -25,7 +26,10 @@ public class DownloadSourceLanguageL33t extends BaseTest {
 
         LP.clickLanguageL33t();
 
+        Assert.assertEquals(
         getDriver().findElement(By.xpath("//p[@style='border: 1px solid #1b651c; background-color: #ffffee; padding: " +
-                "5px; margin: 5px 10px 5px 10px; text-align:center;']/a[@href='/download/963']")).click();
+                "5px; margin: 5px 10px 5px 10px; text-align:center;']/a[@href='/download/963']")).getText(),
+                "Download Source"
+        );
     }
 }
