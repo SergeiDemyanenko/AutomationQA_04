eb1223e738b82e8f653f4478d7ea656327e3d6fa
Irina Sh <104162505+Kononirin@users.noreply.github.com>
2022-07-02 21:25:43 +0500

    Update ConflictResolvingPracticeTest.java

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -5,13 +5,13 @@ import runner.BaseTest;
 public class ConflictResolvingPracticeTest extends BaseTest {
 
   public static String getTextljHEFJEJLFVJLWEVFWVFEF(String text, int number) {
+  
+  number += number;
 
 //    kjdgc Hello,World!
-}
+ }
 
     public static String getTextOneTwoThree(String text) {
-
-        number += number;
 
         return text;
     }
