0074001efbaf8f3d9f30389feac0e8cc4111ba9a
Max <severyanovmax@gmail.com>
2022-07-02 19:26:27 +0300

    conflict

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -9,6 +9,14 @@ public class ConflictResolvingPracticeTest extends BaseTest {
 
     public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
 
+    public void testHistoricInformationLinkText2() {
+        getDriver().get(BASE_URL);
+
+        StartPage startPage = new StartPage(getDriver());
+
+        Assert.assertEquals(startPage.getHistoricInformationLinkText(), "historic information");
+    }
+
     @Test
     public void testHistoricInformationLinkText() {
         getDriver().get(BASE_URL);
