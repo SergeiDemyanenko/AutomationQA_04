e62f5ab67ce0f230b57d22ca2a21a7637dbdd9f1
BOksanaV <BOV1702@gmail.com>
2022-07-02 11:42:10 -0400

    OBakatova/conflict 2

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.StartPage;
 import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
@@ -9,10 +12,23 @@ public class ConflictResolvingPracticeTest extends BaseTest {
 //    kjdgc
 
 
-
+    private static final String BASE_URL = "http://www.99-bottles-of-beer.net";
     public static String getText(String text) {
 
         return text;
+    }
+
+    @Test
+    public void testCheckSubmenuPrivacyIsClickable(){
+        String expectedResult = "Privacy";
+
+        getDriver().get(BASE_URL);
+
+        StartPage startPage = new StartPage(getDriver());
+
+        String actualResult = startPage.getImpressumSubmenuText();
+
+        Assert.assertEquals(actualResult,expectedResult);
     }
 
 
