a29de0f23f9a00f785c136833fe63e5aab66fda5
SvetlanaG1011 <103342736+SvetlanaG1011@users.noreply.github.com>
2022-07-02 09:03:50 -0800

    Update ConflictResolvingPracticeTest.java

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -14,10 +14,7 @@ public class ConflictResolvingPracticeTest extends BaseTest {
 
 public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
 
-    public static String getText(String text) {
-
-        return text;
-    }
+    
 
     public String getText1(WebElement element) {
 
@@ -37,6 +34,7 @@ public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
     }
 
   public static String getText(String text) {
+  
    return text;
   }
 
@@ -46,4 +44,5 @@ public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
 
         return text.concat("Text2").concat("Text3");
 
+}
 }
