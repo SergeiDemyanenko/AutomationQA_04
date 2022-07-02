7d3109eeb6f530b59e4c66cea8e5f1b9cb4e7b1e
ServachakM <servachak.m.u@gmail.com>
2022-07-02 18:25:12 +0300

    US/ConflictingCode - ServachakM

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -1,8 +1,17 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
+
+    private final By H2_TITLE_A = By.xpath("//div[@id='main']/h2");
+
+    public WebElement getH2PageA() {
+
+        return getDriver().findElement(H2_TITLE_A);
+    }
 
 
 
