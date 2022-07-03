52e645bb0147d26b79793cbe96b554e77c99cb8e
VasiliySiniavin <vasinyavin@yandex.ru>
2022-07-03 18:25:59 +0300

    Created test testVerifyTextPleaseNoteSubmitNewLanguage

--- src/test/java/pages/SubmitNewLanguagePage.java
+++ src/test/java/pages/SubmitNewLanguagePage.java
@@ -129,8 +129,14 @@ public class SubmitNewLanguagePage {
 
         return c.replace("https://www.99-bottles-of-beer.net", "");
     }
+
     public WebElement getTitleH3() {
 
         return getDriver().findElement(TEXT_H3);
+    }
+
+    public String getTitleH3Text() {
+
+        return getTitleH3().getText();
     }
 }
