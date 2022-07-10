b346e4ae490e1b3ea7c7c8b892873662beedb514
Max <severyanovmax@gmail.com>
2022-07-11 02:15:46 +0300

    [RF][Structure][LettersPage] Page created LettersPage Pages used in the Refactoring APage CPage DPage IPage JPage

--- src/test/java/pages/browse_languages/letters/CPage.java
+++ src/test/java/pages/browse_languages/letters/CPage.java
@@ -4,42 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class CPage {
+public class CPage extends _LettersPage {
 
-    private final WebDriver driver;
-
-    private final By H2_TITLE_C = By.xpath("//div[@id='main']/h2");
-    private final By DESCRIPTION_C = By.xpath("//div[@id='main']/p[text()]");
     private final By CSHARP_LANGUAGE = By.linkText("C#");
 
-    public CPage(WebDriver existingDriver) {
+    public CPage(WebDriver driver) {
 
-        this.driver = existingDriver;
-   }
-
-    protected WebDriver getDriver() {
-
-        return driver;
-    }
-
-    public WebElement getDescriptionCategoryC() {
-
-        return getDriver().findElement(DESCRIPTION_C);
-    }
-
-    public String getDescriptionCategoryCText() {
-
-        return getDescriptionCategoryC().getText();
-    }
-
-    public WebElement getH2PageC() {
-
-        return getDriver().findElement(H2_TITLE_C);
-    }
-
-    public String getH2PageCText() {
-
-        return getH2PageC().getText();
+        super(driver);
     }
 
     public WebElement getCSharpLanguage() {
