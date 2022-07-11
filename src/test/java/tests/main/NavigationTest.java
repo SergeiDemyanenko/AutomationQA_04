d38b72dc111ef5d6c1b2f7bebfbde2587ce44c0d
SvetlanaG1011 <Sg26025959@gmail.com>
2022-07-10 19:21:33 -0800

    [RF][Main][navigation][menu][submitnewlanguage] Move navigation tests to NavigationTest

--- src/test/java/tests/main/NavigationTest.java
+++ src/test/java/tests/main/NavigationTest.java
@@ -32,4 +32,36 @@ public class NavigationTest extends BaseTest {
 
         Assert.assertEquals(expectedTextH2MainHeader, actualTextH2MainHeader);
     }
+
+    @Test
+    public void testMenuSubmitNewLanguageText() {
+        final String expectedMenuText = "SUBMIT NEW LANGUAGE";
+
+        String actualMenuText =
+                openBaseURL()
+                        .getSubmitNewLanguageMenuText();
+
+        Assert.assertEquals(actualMenuText, expectedMenuText);
+    }
+
+    @Test
+    public void testMenuSubmitNewLanguageLinkText() {
+        final String expectedLinkText = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
+
+        String actualLinkText =
+                openBaseURL()
+                        .getSubmitNewLanguageMenu()
+                        .getAttribute("href");
+
+        Assert.assertEquals(actualLinkText, expectedLinkText);
+    }
+
+    @Test
+    public void testMenuSubmitNewLanguageNavigation() {
+        openBaseURL()
+                .clickSubmitNewLanguageMenu();
+
+        Assert.assertFalse(getDriver().getPageSource().isEmpty());
+        Assert.assertTrue(getDriver().getCurrentUrl().contains("submitnewlanguage"));
+    }
 }
