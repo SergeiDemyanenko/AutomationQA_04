cb03afedb0e74f15722dbf293c993971888d6bf1
MaksPt <100244529+MaksPt@users.noreply.github.com>
2022-07-11 03:14:59 +0300

    Update CPage.java

--- src/test/java/pages/browse_languages/letters/CPage.java
+++ src/test/java/pages/browse_languages/letters/CPage.java
@@ -3,15 +3,38 @@ package pages.browse_languages.letters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import pages.BasePage;
 import pages.browse_languages.languages.LanguageCSharp1614Page;
 
-public class CPage extends _LettersPage {
+public class CPage extends BasePage {
 
+    private final By H2_TITLE_C = By.xpath("//div[@id='main']/h2");
+    private final By DESCRIPTION_C = By.xpath("//div[@id='main']/p[text()]");
     private final By CSHARP_LANGUAGE = By.linkText("C#");
 
     public CPage(WebDriver driver) {
 
         super(driver);
+    }
+
+    public WebElement getDescriptionCategoryC() {
+
+        return getDriver().findElement(DESCRIPTION_C);
+    }
+
+    public String getDescriptionCategoryCText() {
+
+        return getDescriptionCategoryC().getText();
+    }
+
+    public WebElement getH2PageC() {
+
+        return getDriver().findElement(H2_TITLE_C);
+    }
+
+    public String getH2PageCText() {
+
+        return getH2PageC().getText();
     }
 
     public WebElement getCSharpLanguage() {
@@ -24,4 +47,3 @@ public class CPage extends _LettersPage {
 
         return new LanguageCSharp1614Page(getDriver());
     }
-}
