7cc045e4bdff81b1149209bdad329908c088a284
new_name <new_email>
2022-07-13 11:59:00 +0200

    [RF][Structure][APage]Created ATest <OlgaSaf>

--- src/test/java/tests/_to_do/OlgaSafMenuStartPageTest.java
+++ src/test/java/tests/_to_do/OlgaSafMenuStartPageTest.java
@@ -106,40 +106,6 @@ public class OlgaSafMenuStartPageTest extends BaseTest {
 
 
     @Test
-    public void testSubmenuAbcTitle()  {
-        String expectedResult = "A";
-
-        getDriver().get(URL);
-
-        MainPage main = new MainPage(getDriver());
-
-        main.clickBrowseLanguagesMenu();
-
-        AbcPage abcA = new AbcPage(getDriver());
-
-        String actualResult = abcA.getASubmenu().getText();
-
-          Assert.assertEquals(actualResult, expectedResult);
-    }
-
-    @Test
-    public void testVerifyLanguageAlgol60() {
-        String expectedResult = "Algol60";
-
-        getDriver().get(URL);
-
-        MainPage bl = new MainPage(getDriver());
-        bl.clickBrowseLanguagesMenu();
-
-        AbcPage abcPage = new AbcPage(getDriver());
-        abcPage.clickASubmenu();
-
-        String actualResult = getDriver().findElement(
-                By.xpath("//td[@bgcolor='#efefef']/a[text()='Algol60']")).getText();
-        Assert.assertEquals(actualResult, expectedResult);
-    }
-
-    @Test
     public void testClickAllLinksTeamPage() {
 
         List<String> expectedResult = new ArrayList<>();
