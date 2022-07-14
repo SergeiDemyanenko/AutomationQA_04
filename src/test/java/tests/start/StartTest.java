9aae1d92f1f5dbc237a2762f3d76ca084098b061
Aleksandr Grazhdankin <grazhdankin.qa@gmail.com>
2022-07-14 21:49:08 +0300

    Deleted 3 own classes
    RF metods in StartPage class
    
    Added tests from removed classes in StartTest Class

--- src/test/java/tests/start/StartTest.java
+++ src/test/java/tests/start/StartTest.java
@@ -1,7 +1,5 @@
 package tests.start;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -42,5 +40,130 @@ public class StartTest extends BaseTest {
                 .getH2HeaderMainText();
 
         Assert.assertEquals(actualH2Header, expectedH2Header);
+    }
+
+    @Test
+    public void testHistoricInformationLinkText() {
+        String expectedHistoricInformationLinkText = "historic information";
+
+        String actualHistoricInformationLinkText =
+                openBaseURL()
+                        .clickStartMenu()
+                        .getHistoricInformationLinkText();
+
+        Assert.assertEquals(actualHistoricInformationLinkText, expectedHistoricInformationLinkText);
+    }
+
+    @Test
+    public void testHistoricInformationLink() {
+        String expectedHistoricInformationLinkTitle = "99 Bottles of Beer | Background and historic information";
+
+        String actualHistoricInformationLinkTitl =
+                openBaseURL()
+                        .clickStartMenu()
+                        .clickHistoricInformationLink()
+                        .getTitle();
+
+        Assert.assertEquals(actualHistoricInformationLinkTitl, expectedHistoricInformationLinkTitle);
+    }
+
+    @Test
+    public void testHereLinkText() {
+        String expectedHereLinkText = "here";
+
+        String actualHereLinkTextText =
+                openBaseURL()
+                        .clickStartMenu()
+                        .getHereLinkText();
+
+        Assert.assertEquals(actualHereLinkTextText, expectedHereLinkText);
+    }
+
+    @Test
+    public void testHereLink() {
+        String expectedHereLinkTitle = "99 Bottles of Beer | The lyrics to the song 99 Bottles of Beer";
+
+        String actualHereLinkTitle =
+                openBaseURL()
+                        .clickStartMenu()
+                        .clickHereLink()
+                        .getTitle();
+
+        Assert.assertEquals(actualHereLinkTitle, expectedHereLinkTitle);
+    }
+
+    @Test
+    public void testSubmitYourOwnPieceOfCodeLinkText() {
+        String expectedSubmitYourOwnPieceOfCodeLinkText = "submit your own piece of code";
+
+        String actualSubmitYourOwnPieceOfCodeLinkText =
+                openBaseURL()
+                        .clickStartMenu()
+                        .getSubmitYourOwnPieceOfCodeLinkText();
+
+        Assert.assertEquals(actualSubmitYourOwnPieceOfCodeLinkText, expectedSubmitYourOwnPieceOfCodeLinkText);
+    }
+
+    @Test
+    public void testSubmitYourOwnPieceOfCodeLink() {
+        String expectedSubmitYourOwnPieceOfCodeLinkTitle = "99 Bottles of Beer | Submit new Language";
+
+        String actualSubmitYourOwnPieceOfCodeLinkTitle =
+                openBaseURL()
+                        .clickStartMenu()
+                        .clickSubmitYourOwnPieceOfCodeLink()
+                        .getTitle();
+
+        Assert.assertEquals(actualSubmitYourOwnPieceOfCodeLinkTitle, expectedSubmitYourOwnPieceOfCodeLinkTitle);
+    }
+
+    @Test
+    public void testGuestbookLinkText() {
+        String expectedGuestbookLinkText = "guestbook";
+
+        String actualGuestbookLinkText =
+                openBaseURL()
+                        .clickStartMenu()
+                        .getGuestbookLinkText();
+
+        Assert.assertEquals(actualGuestbookLinkText, expectedGuestbookLinkText);
+    }
+
+    @Test
+    public void testGuestbookLink() {
+        String expectedGuestbookLinkTitle = "99 Bottles of Beer | Guestbook";
+
+        String actualGuestbookLinkTitle =
+                openBaseURL()
+                        .clickStartMenu()
+                        .clickGuestbookLink()
+                        .getTitle();
+
+        Assert.assertEquals(actualGuestbookLinkTitle, expectedGuestbookLinkTitle);
+    }
+
+    @Test
+    public void testTeamMembersLinkText() {
+        String expectedTeamMembersLinkText = "team members";
+
+        String actualTeamMembersLinkText =
+                openBaseURL()
+                        .clickStartMenu()
+                        .getTeamMembersLinkText();
+
+        Assert.assertEquals(actualTeamMembersLinkText, expectedTeamMembersLinkText);
+    }
+
+    @Test
+    public void testTeamMembersLink() {
+        String expectedTeamMembersLinkTitle = "99 Bottles of Beer | The Team";
+
+        String actualTeamMembersLinkTitle =
+                openBaseURL()
+                        .clickStartMenu()
+                        .clickTeamMembersLink()
+                        .getTitle();
+
+        Assert.assertEquals(actualTeamMembersLinkTitle, expectedTeamMembersLinkTitle);
     }
 }
