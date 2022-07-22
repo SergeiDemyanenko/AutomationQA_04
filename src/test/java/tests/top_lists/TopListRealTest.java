9c5c941fdac0dbf09c29dedd3847cc5128d4ccc9
ServachakM <servachak.m.u@gmail.com>
2022-07-22 23:14:27 +0300

    added test HeaderTextSchemeLanguage

--- src/test/java/tests/top_lists/TopListRealTest.java
+++ src/test/java/tests/top_lists/TopListRealTest.java
@@ -90,15 +90,30 @@ public class TopListRealTest extends BaseTest {
     @Test
     public void testUrlSchemeLanguage() {
         final String expectedUrl = "http://www.99-bottles-of-beer.net/language-scheme-582.html";
-        final String Schemelanguage = "Scheme (original version)";
+        final String SchemeLanguage = "Scheme (original version)";
 
-        String actualTitle =
+        String actualUrl =
                 openBaseURL()
                         .clickTopListsMenu()
                         .clickTopListRealSubmenu()
-                        .clickLanguage(Schemelanguage)
+                        .clickLanguage(SchemeLanguage)
                         .getURL();
 
-        Assert.assertEquals(actualTitle, expectedUrl);
+        Assert.assertEquals(actualUrl, expectedUrl);
+    }
+
+    @Test
+    public void testHeaderTextSchemeLanguage() {
+        final String expectedHeader = "Language Scheme";
+        final String SchemeLanguage = "Scheme (original version)";
+
+        String actualHeader =
+                openBaseURL()
+                        .clickTopListsMenu()
+                        .clickTopListRealSubmenu()
+                        .clickLanguage(SchemeLanguage)
+                        .getH2MainText();
+
+        Assert.assertEquals(actualHeader, expectedHeader);
     }
 }
